98a241d903dec42bbdced649279c4284a7927be1
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-05-07 02:56:27 +0900

    chore: ESLintを設定した

--- ess-app/eslint.config.mjs
+++ ess-app/eslint.config.mjs
@@ -11,6 +11,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+
 ];
 
 export default eslintConfig;
